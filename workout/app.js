e719c83f08b3893bc983cece015d375989cd1ccb
patsonm <38109001+patsonm@users.noreply.github.com>
2019-05-30 09:56:44 -0400

    Workout Application App Using Handlebars

---
+++ workout/app.js
@@ -0,0 +1,198 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var session = require('express-session');
+var bodyParser = require('body-parser');
+app.use(express.static('public'));
+
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    host  : 'mysql.eecs.oregonstate.edu',
+    user  : 'cs290_patsonm',
+    password: '9171',
+    database: 'cs290_patsonm',
+    dateStrings: 'true'
+});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 4234);
+
+app.get('/', function(req, res, next){
+    var context = {};
+    pool.query('SELECT * FROM workouts', function(err, rows, fields){
+    if(err){
+        next(err);
+        return;
+    }
+    var list = [];
+    for(var row in rows){
+        var toPush = {'name': rows[row].name, 
+                    'reps': rows[row].reps, 
+                    'weight': rows[row].weight, 
+                    'date':rows[row].date, 
+                    'id':rows[row].id};
+        if(rows[row].lbs){
+            toPush.lbs = "LBS";
+        }
+        else{
+            toPush.lbs = "KG";
+        }
+        list.push(toPush);
+    }
+    context.results = list;
+    res.render('home', context);
+    })
+});
+
+app.get('/reset-table',function(req,res,next){
+    var context = {};
+    pool.query("DROP TABLE IF EXISTS workouts", function(err){
+        var createString = "CREATE TABLE workouts("+
+        "id INT PRIMARY KEY AUTO_INCREMENT,"+
+        "name VARCHAR(255) NOT NULL,"+
+        "reps INT,"+
+        "weight INT,"+
+        "date DATE,"+
+        "lbs BOOLEAN)";
+        pool.query(createString, function(err){
+            res.render('home',context);
+        })
+    });
+});
+
+app.get('/insert',function(req,res,next){
+  var context = {};
+  pool.query("INSERT INTO `workouts` (`name`, `reps`, `weight`, `date`, `lbs`) VALUES (?, ?, ?, ?, ?)", 
+    [req.query.exercise, 
+    req.query.reps, 
+    req.query.weight, 
+    req.query.date, 
+    req.query.measurement], 
+    function(err, result){
+        if(err){
+          next(err);
+          return;
+        } 
+        context.inserted = result.insertId;
+        res.send(JSON.stringify(context));
+  });
+});
+
+app.get('/delete', function(req, res, next) {
+    var context = {};
+    pool.query("DELETE FROM `workouts` WHERE id = ?", 
+        [req.query.id], 
+        function(err, result) {
+            if(err){
+                next(err);
+                return;
+            }
+    });
+});
+
+app.get('/update',function(req, res, next){
+    var context = {};
+    pool.query('SELECT * FROM `workouts` WHERE id=?',
+        [req.query.id], 
+        function(err, rows, fields){
+            if(err){
+                next(err);
+                return;
+            }
+            var list = [];
+
+            for(var row in rows){
+                var toPush = {'name': rows[row].name, 
+                            'reps': rows[row].reps, 
+                            'weight': rows[row].weight, 
+                            'date':rows[row].date, 
+                            'lbs':rows[row].lbs,
+                            'id':rows[row].id};
+
+                list.push(toPush);
+            }
+        context.results = list[0];
+        res.render('update', context);
+    });
+});
+
+app.get('/updateBack', function(req, res, next){
+    var context = {};
+
+    pool.query("SELECT * FROM `workouts` WHERE id=?", 
+        [req.query.id], 
+        function(err, result){
+            if(err){
+                next(err);
+                return;
+            }
+            if(result.length == 1){
+                var curVals = result[0];
+
+                if(req.query.measurement === "on"){
+                    req.query.measurement = "1";
+                }
+                else{
+                    req.query.measurement = "0";
+                }
+
+                pool.query('UPDATE `workouts` SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=?', 
+                [req.query.exercise || curVals.name, 
+                req.query.reps || curVals.reps, 
+                req.query.weight || curVals.weight, 
+                req.query.date || curVals.date, 
+                req.query.measurement, 
+                req.query.id],
+                function(err, result){
+                    if(err){
+                        next(err);
+                        return;
+                    }
+                    pool.query('SELECT * FROM `workouts`', function(err, rows, fields){
+                        if(err){
+                            next(err);
+                            return;
+                        }
+                        var list = [];
+
+                        for(var row in rows){
+                            var toPush = {'name': rows[row].name, 
+                            'reps': rows[row].reps,
+                            'weight': rows[row].weight, 
+                            'date':rows[row].date, 
+                            'id':rows[row].id};
+
+                            if(rows[row].lbs){
+                                toPush.lbs = "LBS";
+                            }
+                            else{
+                                toPush.lbs = "KG";
+                            }
+                            list.push(toPush);
+                        }
+
+                        context.results = list;
+                        res.render('home', context);
+                    });
+                });
+            }
+    });
+});
+
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
---
+++ workout/public/script.js
@@ -0,0 +1,124 @@
+document.getElementById('addItemSubmit').addEventListener('click',function(event){
+	
+	var addExercise = document.getElementById("newExercise");
+
+	var req = new XMLHttpRequest();
+
+	var qString = "/insert";
+	var parameters = "exercise="+addExercise.elements.exercise.value+
+						"&reps="+addExercise.elements.reps.value+
+						"&weight="+addExercise.elements.weight.value+
+						"&date="+addExercise.elements.date.value;
+	if(addExercise.elements.measurement.checked){
+		parameters+="&measurement=1";
+	}
+	else{
+		parameters+="&measurement=0";
+	}
+	req.open("GET", qString +"?"+parameters, true);
+	req.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+
+	req.addEventListener('load', function(){
+		if(req.status >= 200 && req.status < 400){
+
+			var response = JSON.parse(req.responseText);
+			var id = response.inserted;
+
+			var table = document.getElementById("dataTable");
+
+			var row = table.insertRow(-1);
+
+			var exerciseCell = document.createElement('td');
+			exerciseCell.textContent = addExercise.elements.exercise.value;
+			row.appendChild(exerciseCell);
+
+			var repCell = document.createElement('td');
+			repCell.textContent = addExercise.elements.reps.value;
+			row.appendChild(repCell);
+
+			var weightCell = document.createElement('td');
+			weightCell.textContent = addExercise.elements.weight.value;
+			row.appendChild(weightCell);
+
+			var measurementCell = document.createElement('td');
+			if(addExercise.elements.measurement.checked){
+				measurementCell.textContent = "LBS"
+			}
+			else{
+				measurementCell.textContent = "KG"
+			}
+			row.appendChild(measurementCell);
+
+			var dateCell = document.createElement('td');
+			dateCell.textContent = addExercise.elements.date.value;
+			row.appendChild(dateCell);
+
+
+			var updateCell = document.createElement('td');
+			var updateLink = document.createElement('a');
+			updateLink.setAttribute('href','/update?id='+id);
+			var updateButton = document.createElement('input');
+			updateButton.setAttribute('type','button');
+			updateButton.setAttribute('value','Update');
+			updateLink.appendChild(updateButton);
+			updateCell.appendChild(updateLink);
+			row.appendChild(updateCell);
+
+			var deleteCell = document.createElement('td');
+			var deleteButton = document.createElement('input');
+			deleteButton.setAttribute('type','button');
+			deleteButton.setAttribute('name','delete');
+			deleteButton.setAttribute('value','Delete');
+			deleteButton.setAttribute('onClick', 'deleteRow("dataTable",'+id+')');
+			var deleteHidden = document.createElement('input');
+			deleteHidden.setAttribute('type','hidden');
+			deleteHidden.setAttribute('id', 'delete'+id);
+			deleteCell.appendChild(deleteButton);
+			deleteCell.appendChild(deleteHidden);
+			row.appendChild(deleteCell);
+
+
+		}
+		else {
+	    	console.log('there was an error');
+		}
+	});
+	
+	req.send(qString +"?"+parameters);
+	event.preventDefault();
+});
+
+function deleteRow(tableId, id){
+	var table = document.getElementById(tableId);
+	var rowCount = table.rows.length;
+
+	var deleteString = "delete"+id;
+
+
+	for(var i = 1; i < rowCount; i++){
+		var row = table.rows[i];
+		var dataCells = row.getElementsByTagName("td");
+		var deleteCell = dataCells[dataCells.length -1];
+		if(deleteCell.children[1].id === deleteString){
+			table.deleteRow(i);
+		}
+
+	}
+
+
+	var req = new XMLHttpRequest();
+	var qString = "/delete";
+
+	req.open("GET", qString+"?id="+id, true);
+
+	req.addEventListener("load",function(){
+		if(req.status >= 200 && req.status < 400){
+	    	console.log('delete request sent');
+		} else {
+		    console.log('there was an error');
+		}
+	});
+
+	req.send(qString+"?id="+id );
+
+}
